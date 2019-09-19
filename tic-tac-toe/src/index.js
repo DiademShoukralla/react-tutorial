3a8806265f282cd3cd989bf5d892bbadfd5552dd
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2019-09-19 11:34:46 -0400

    Fixed line indents

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -78,45 +78,46 @@ class Board extends React.Component {
     }
 }
   
-  class Game extends React.Component {
+class Game extends React.Component {
+
     render() {
-      return (
+        return (
         <div className="game">
-          <div className="game-board">
+            <div className="game-board">
             <Board />
-          </div>
-          <div className="game-info">
+            </div>
+            <div className="game-info">
             <div>{/* status */}</div>
             <ol>{/* TODO */}</ol>
-          </div>
+            </div>
         </div>
-      );
+        );
     }
-  }
+}
   
-  // ========================================
-  
-  ReactDOM.render(
+// ========================================
+
+ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
-  
-  function calculateWinner(squares) {
+);
+
+function calculateWinner(squares) {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
-      }
+        }
     }
     return null;
-  }
+}
